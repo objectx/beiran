799b129efe2226512fbf99388414791c1e2480a9
Alkim Gozen <alkimake@gmail.com>
2017-12-11 15:49:11 +0900

    removed syncio, support async.io
    Due to discussions on connection method between nodes. Syncio removed in
    this branch.
    Also tried to run zeroConf on python 3 async.

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,12 @@ FROM centos:7
 LABEL maintainer="alkim@rlab.io"
 
 RUN yum install -y epel-release; \
-    yum install -y python-pip python-devel gcc; \
-    yum install -y yum-utils device-mapper-persistent-data lvm2;
+    yum install -y gcc yum-utils device-mapper-persistent-data lvm2;
+
+RUN yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+RUN yum -y install python36u python36u-pip python36u-devel
+
+RUN ln -s /usr/bin/pip3.6 /usr/bin/pip
 
 RUN yum-config-manager \
     --add-repo \
@@ -16,7 +20,7 @@ RUN mkdir /src
 WORKDIR /src
 
 ADD [ "requirements.txt", "/src"]
-RUN pip install --upgrade pip
+#RUN pip3.6 install --upgrade pip
 RUN pip install -r requirements.txt
 
 ADD [ "src", "/src" ]
--- src/beiran.py
+++ src/beiran.py
@@ -1,50 +1,16 @@
+import asyncio
 import logging
-import socket
-
 import signal
+import socket
 import sys
-import time
-from threading import Lock
-
 from zeroconf import ServiceBrowser, ServiceInfo, ServiceStateChange, Zeroconf
 
-from flask import Flask, request
-from flask_socketio import SocketIO as SocketIOServer, emit
-from flask_socketio import Namespace
-
-from socketIO_client import SocketIO, LoggingNamespace
-
-import logging
 logging.getLogger('socketIO-client').setLevel(logging.DEBUG)
 logging.basicConfig()
 
 info = None
 hostname = socket.gethostname()
 domain = "_beiran._tcp.local."
-
-app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'secret!'
-
-# Set this variable to "threading", "eventlet" or "gevent" to test the
-# different async modes, or leave it set to None for the application to choose
-# the best option based on installed packages.
-async_mode = None
-
-socketio_server = SocketIOServer(app, port=5000, async_mode=async_mode)
-
-
-thread = None
-thread_lock = Lock()
-
-def background_thread():
-    """Example of how to send server generated events to clients."""
-    count = 0
-    while True:
-        socketio_server.sleep(10)
-        count += 1
-        socketio_server.emit('my_response',
-                      {'data': 'Server generated event', 'count': count},
-                      namespace='/test')
 
 
 def signal_term_handler(signal, frame):
@@ -53,23 +19,24 @@ def signal_term_handler(signal, frame):
         zeroconf.unregister_service(info)
     sys.exit(0)
 
+
 signal.signal(signal.SIGTERM, signal_term_handler)
 
 
 def on_connect():
     print('connect')
 
+
 def on_disconnect():
     print('disconnect')
+
 
 def on_reconnect():
     print('reconnect')
 
+
 def connecNodeWs(info):
-    socketIO = SocketIO(socket.inet_ntoa(info.address), 5000)
-    socketIO.on('connect', on_connect)
-    socketIO.on('disconnect', on_disconnect)
-    socketIO.on('reconnect', on_reconnect)
+    pass
 
 
 def on_service_state_change(zeroconf, service_type, name, state_change):
@@ -79,11 +46,11 @@ def on_service_state_change(zeroconf, service_type, name, state_change):
         info = zeroconf.get_service_info(service_type, name)
         # if name != hostname + "." + domain:
         connecNodeWs(info)
-            
+
         if info:
             print("  Address: %s:%d" % (socket.inet_ntoa(info.address), info.port))
             print("  Weight: %d, priority: %d" % (info.weight, info.priority))
-            print("  Server: %s" % (info.server))
+            print("  Server: %s" % info.server)
             if info.properties:
                 print("  Properties are:")
                 for key, value in info.properties.items():
@@ -95,6 +62,35 @@ def on_service_state_change(zeroconf, service_type, name, state_change):
         print('\n')
 
 
+async def discover():
+    global info, zeroconf
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('google.com', 0))
+    hostip = s.getsockname()[0]
+    print("hostname = " + hostname)
+    print("ip = " + hostip)
+    desc = {'name': hostname}
+    info = ServiceInfo(domain,
+                       hostname + "." + domain,
+                       socket.inet_aton(hostip), 3000, 0, 0,
+                       desc, hostname + ".local.")
+    zeroconf = Zeroconf()
+    print("Registration of a service, press Ctrl-C to exit...")
+    try:
+        print("Registering " + hostname + "...")
+        zeroconf.register_service(info)
+    except KeyboardInterrupt:
+        pass
+    print("\nBrowsing services, press Ctrl-C to exit...\n")
+    browser = ServiceBrowser(zeroconf, domain, handlers=[on_service_state_change])
+    try:
+        while True:
+            await asyncio.sleep(1)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        zeroconf.close()
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
@@ -104,66 +100,7 @@ if __name__ == '__main__':
         assert sys.argv[1:] == ['--debug']
         logging.getLogger('zeroconf').setLevel(logging.DEBUG)
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(('google.com', 0))
-    hostip = s.getsockname()[0]
-
-    print("hostname = " + hostname)
-    print("ip = " + hostip)
-
-    desc = {'name': hostname}
-    info = ServiceInfo(domain,
-                       hostname + "." + domain,
-                       socket.inet_aton(hostip), 3000, 0, 0,
-                       desc, hostname + ".local.")
-
-    zeroconf = Zeroconf()
-    print("Registration of a service, press Ctrl-C to exit...")
-
-    try:
-        print("Registering " + hostname + "...")
-        zeroconf.register_service(info)
-    except KeyboardInterrupt:
-        pass
-
-    print("\nBrowsing services, press Ctrl-C to exit...\n")
-    browser = ServiceBrowser(zeroconf, domain, handlers=[on_service_state_change])
-
-    try:
-        while True:
-            time.sleep(0.1)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        zeroconf.close()
-
-
-@socketio_server.on_error()        # Handles the default namespace
-def error_handler(e):
-    pass
-
-@socketio_server.on_error_default  # handles all namespaces without an explicit error handler
-def default_error_handler(e):
-    pass
-
-
-
-class BeiranNamespace(Namespace):
-
-    def on_my_ping(self):
-        emit('my_pong')
-
-    def on_connect(self):
-        global thread
-        with thread_lock:
-            if thread is None:
-                thread = socketio_server.start_background_task(
-                    target=background_thread)
-        emit('my_response', {'data': 'Connected', 'count': 0})
-
-    def on_disconnect(self):
-        print('Client disconnected', request.sid)
-
-
-socketio_server.on_namespace(BeiranNamespace('/beiran'))
-
+    loop = asyncio.get_event_loop()
+    # Blocking call which returns when the display_date() coroutine is done
+    loop.run_until_complete(discover())
+    loop.close()

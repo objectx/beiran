ead8b88c8fabc7d4dc20eb617c283c22d6d627c7
Ali Riza Keles <ali.r.keles@gmail.com>
2018-01-25 00:47:12 +0300

    network info methods are moved to daemon's lib module

--- beirand/beirand/lib.py
+++ beirand/beirand/lib.py
@@ -9,9 +9,11 @@ import os
 import socket
 import tarfile
 from uuid import uuid4
+
 from beiran.log import build_logger
 
 LOGGER = build_logger()
+
 
 def docker_sha_summary(sha):
     """
@@ -205,3 +207,103 @@ def local_node_uuid():
 
     LOGGER.info("nodes UUID is: %s", uuid)
     return uuid
+
+
+def get_default_gateway_ip_interface():
+    """
+    Get default gateway's ip and interface info.
+
+    Returns:
+        tuple: ip address, interface name. (10.0.2.2, eth0)
+
+    """
+    return netifaces.gateways()['default'][netifaces.AF_INET]
+
+
+def get_listen_address():
+    """
+
+    Returns:
+        string: daemon listen IP address
+
+    """
+    env_addr = ''
+
+    try:  # try to get from environment variable
+        env_addr = os.environ['LISTEN_ADDR']
+        listen_address = ipaddress.ip_address(env_addr)
+        return listen_address.compressed
+
+    except KeyError:  # if env var is not set
+        listen_interface = get_listen_interface()
+        return netifaces.ifaddresses(listen_interface)[netifaces.AF_INET][0]['addr']
+
+    except ValueError:  # if env var is set erroneously
+        raise ValueError("""Please check environment variable LISTEN_ADDR,
+                         it must be a valid IP4 address. `{}` is not a valid one!""".format(env_addr))
+
+
+def get_listen_interface():
+    """
+    Seek for listen interface in order described below and return it.
+
+    First try LISTEN_INTERFACE env var.
+    Second try to find the interface of ip address specified by LISTEN_ADDR env var.
+    Third, if LISTEN_ADDR is not set return default gateway's interface
+
+    Returns
+        string: listen address.
+
+    """
+
+    if 'LISTEN_INTERFACE' in os.environ:
+        return os.environ['LISTEN_INTERFACE']
+
+    if 'LISTEN_ADDR' in os.environ:
+        for interface in netifaces.interfaces():
+            for ip in netifaces.ifaddresses(interface)[netifaces.AF_INET]:
+                if ip == os.environ.get('LISTEN_ADDR'):
+                    return interface
+
+        raise ValueError("Your LISTEN_ADDR does not match any network interface!")
+
+    _, interface = get_default_gateway_ip_interface()
+
+    return interface
+
+
+def get_advertise_address():
+    """
+    First try environment variable `ADVERTISE_ADDR`. If it is not set,
+    return the listen address unless it is `0.0.0.0`.
+
+    Lastly return default gateway's ip address
+
+    Returns
+        string: listen address.
+
+
+    Returns:
+        string: ip address of advertise address
+
+    """
+
+    if 'ADVERTISE_ADDR' in os.environ:
+        return os.environ['ADVERTISE_ADDR']
+
+    listen_address = get_listen_address()
+
+    if listen_address != '0.0.0.0':
+        return listen_address
+
+    ip, _ = get_default_gateway_ip_interface()
+
+    return ip
+
+
+def get_hostname(self):
+    """ Gets hostname for discovery
+    """
+    if 'HOSTNAME' in os.environ:
+        return os.environ['HOSTNAME']
+    return socket.gethostname()

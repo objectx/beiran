f54566d30878e49b2c06944a3364128f7e2cb680
Ali Riza Keles <ali.r.keles@gmail.com>
2018-02-25 16:24:00 +0300

    add a new json string field object extending text field of peewee. add `details` field to dump all docker daemon information.

--- beiran/models/docker.py
+++ beiran/models/docker.py
@@ -2,7 +2,7 @@
 Module for Docker Daemon Data Model
 """
 from peewee import CharField, ForeignKeyField
-from beiran.models.base import BaseModel
+from beiran.models.base import BaseModel, JSONStringField
 from beiran.models.node import Node
 
 
@@ -14,3 +14,4 @@ class DockerDaemon(BaseModel):
     docker_version = CharField(max_length=20)
     storage_driver = CharField(max_length=50)  # overlay2, aufs
     docker_root_dir = CharField(default='/var/lib/docker')
+    details = JSONStringField()  # dump all data from docker_client.info()

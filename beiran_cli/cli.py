2f0c415bac1d51ae0859b5d07c7cc51a32223ed9
Furkan Mustafa <furkan@s2n.io>
2018-04-23 13:40:33 +0900

    [cli] report docker version of nodes too

--- beiran_cli/cli.py
+++ beiran_cli/cli.py
@@ -96,7 +96,13 @@ class Cli:
     def node_list(self, all_nodes):
         """List known beiran nodes"""
         nodes = self.beiran_client.get_nodes(all_nodes=all_nodes)
-        table = [[n['uuid'], n['ip_address'] + ':' + str(n['port']), 'N/A', 'OK'] for n in nodes]
+        table = []
+        for n in nodes:
+            if 'docker' in n and n['docker']:
+                docker_version = n['docker']['ServerVersion']
+            else:
+                docker_version = 'N/A'
+            table.append([n['uuid'], n['ip_address'] + ':' + str(n['port']), docker_version, 'OK'])
         print(tabulate(table, headers=["UUID", "IP:Port", "Docker Ver.", "Status?"]))
 
     node.add_command(node_list)

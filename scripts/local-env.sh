6a397ae9fba75b0b9386a3b641ff3db62c7a9331
Furkan Mustafa <furkan@s2n.io>
2018-04-25 18:24:51 +0900

    cleanup before release

--- scripts/local-env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-export BEIRAN_ROOT=$(git rev-parse --show-toplevel)
-echo "Detected beiran root dir: ${BEIRAN_ROOT}"
-
-mkdir -p ${BEIRAN_ROOT}/run || true
-export BEIRAN_SOCK=${BEIRAN_ROOT}/run/beirand.sock

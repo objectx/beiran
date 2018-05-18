bf28c9553ffa70b105d630a74fef779ff24d10a1
Ali Riza Keles <ali.r.keles@gmail.com>
2018-05-18 21:35:45 +0300

    async post method with aiohttp added in beiran.lib module

--- beiran/lib.py
+++ beiran/lib.py
@@ -19,3 +19,22 @@ async def async_fetch(url, timeout=3, **kwargs):
             async with session.get(url, headers=kwargs) as resp:
                 response, data = resp, await resp.json()
                 return response, data
+
+
+async def async_post(url, data, timeout=3, **kwargs):
+    """
+    Async http post with aiohttp
+    Args:
+        url (str): post url
+        data (dict): post request data
+        timeout (int): timeout
+
+    Returns:
+        (ClientResponse, dict): response instance, response json
+
+    """
+    async with aiohttp.ClientSession() as session:
+        async with async_timeout.timeout(timeout):
+            async with session.post(url, data=data, headers=kwargs) as resp:
+                response, data = resp, await resp.json()
+                return response, data

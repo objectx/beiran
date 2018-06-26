44fb3a177f0d946ed5e76a32713d7fb06e5ec4cf
Ali Riza Keles <ali.r.keles@gmail.com>
2018-06-26 06:53:00 +0300

    a new lib method `build_node_address` which builds a node address with given, host, uuid,port protocol

--- beiran/lib.py
+++ beiran/lib.py
@@ -25,3 +25,25 @@ async def async_req(url, timeout=3, method="GET", **kwargs):
                                        data=data, headers=headers) as resp:
                 response, data = resp, await resp.json(content_type=None)
                 return response, data
+
+
+ADDRESS_FORMAT = "beiran+{protocol}://{hostname}:{port}#{uuid}"
+
+def build_node_address(host, uuid=None, port=8888, protocol="http"):
+    """
+    Build a node address with given host, port, protocol and uuid
+
+    Args:
+        host: hostname
+        uuid: uuid of node
+        port: service port
+        protocol: protocol, default http
+
+    Returns:
+
+    """
+    address = ADDRESS_FORMAT.format(host=host, port=port, protocol=protocol, uuid=uuid)
+    if not uuid:
+        address = address.split('#')[0]
+
+    return address

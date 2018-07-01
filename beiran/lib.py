e65949e1349f907ce4ce2c8b526214ddd1413d98
Ali Riza Keles <ali.r.keles@gmail.com>
2018-07-02 02:01:19 +0300

    remove redundant method build_node_address from beiran.lib

--- beiran/lib.py
+++ beiran/lib.py
@@ -26,25 +26,3 @@ async def async_req(url, timeout=3, method="GET", **kwargs):
                 response, data = resp, await resp.json(content_type=None)
                 return response, data
 
-
-ADDRESS_FORMAT = "beirans+{protocol}://{hostname}:{port}#{uuid}"
-
-def build_node_address(host, uuid=None, port=None, protocol="http"):
-    """
-    Build a node address with given host, port, protocol and uuid
-
-    Args:
-        host: hostname
-        uuid: uuid of node
-        port: service port
-        protocol: protocol, default http
-
-    Returns:
-
-    """
-    port = port or 8888
-    address = ADDRESS_FORMAT.format(hostname=host, port=port, protocol=protocol, uuid=uuid)
-    if not uuid:
-        address = address.split('#')[0]
-
-    return address

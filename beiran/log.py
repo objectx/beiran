9625ec06e2d2090f24ba330f700c29c047722601
s-nakamura <s-nakamura@creationline.com>
2018-07-07 16:08:37 +0900

    mypy: added new type annotation (beiran/log.py)

--- beiran/log.py
+++ beiran/log.py
@@ -4,14 +4,16 @@ import os
 import sys
 import logging
 
+from typing import List, Union # pylint: disable=unused-import
+
 def build_logger(filename: str = None, log_level: int = logging.DEBUG) -> logging.Logger:
     """ Build logger class for module """
     stdout_handler = logging.StreamHandler(sys.stdout)
-    handlers = [stdout_handler]
+    handlers = [stdout_handler] # type: List[Union[logging.FileHandler, logging.StreamHandler]]
     if filename:
         file_handler = logging.FileHandler(filename=os.getenv(
             'LOG_FILE', filename))
-        handlers.append(file_handler) # type: ignore
+        handlers.append(file_handler)
     logging.getLogger('asyncio').level = logging.WARNING
     logging.basicConfig(
         level=log_level,

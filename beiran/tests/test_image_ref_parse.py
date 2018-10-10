3e396afc7bd1b848bbd6cb86dbdcbb0245d3f5b6
s-nakamura <s-nakamura@creationline.com>
2018-10-10 15:55:24 +0900

    docker: add image_ref_parse.py and enable to normalize image reference #157

---
+++ beiran/tests/test_image_ref_parse.py
@@ -0,0 +1,33 @@
+import pytest
+from beiran_package_docker.image_ref_parse import normalize_ref
+
+@pytest.mark.parametrize('ref,normalized', [
+    ('ubuntu', 'docker.io/library/ubuntu:latest'),
+    ('ubuntu:14.04', 'docker.io/library/ubuntu:14.04'),
+    ('repo/ubuntu', 'docker.io/repo/ubuntu:latest'),
+    ('repo/ubuntu:14.04', 'docker.io/repo/ubuntu:14.04'),
+    ('path/repo/ubuntu:14.04', 'docker.io/path/repo/ubuntu:14.04'),
+
+    ('domain.com/repo/ubuntu', 'domain.com/repo/ubuntu:latest'),
+    ('domain.com/repo/ubuntu:14.04', 'domain.com/repo/ubuntu:14.04'),
+    ('domain.com/path/repo/ubuntu:14.04', 'domain.com/path/repo/ubuntu:14.04'),
+
+    ('localhost:8080/repo/ubuntu', 'localhost:8080/repo/ubuntu:latest'),
+    ('localhost:8080/repo/ubuntu:14.04', 'localhost:8080/repo/ubuntu:14.04'),
+    ('localhost:8080/path/repo/ubuntu:14.04', 'localhost:8080/path/repo/ubuntu:14.04'),
+
+    ('ubuntu@sha256:cb96ec8eb632c873d5130053cf5e2548234e5275d8115a39394289d96c9963a6',
+     'docker.io/library/ubuntu@sha256:cb96ec8eb632c873d5130053cf5e2548234e5275d8115a39394289d96c9963a6'),
+    ('repo/ubuntu@sha256:cb96ec8eb632c873d5130053cf5e2548234e5275d8115a39394289d96c9963a6',
+     'docker.io/repo/ubuntu@sha256:cb96ec8eb632c873d5130053cf5e2548234e5275d8115a39394289d96c9963a6'),
+    ('domain.com/repo/ubuntu@sha256:cb96ec8eb632c873d5130053cf5e2548234e5275d8115a39394289d96c9963a6', 
+     'domain.com/repo/ubuntu@sha256:cb96ec8eb632c873d5130053cf5e2548234e5275d8115a39394289d96c9963a6'),
+    ('localhost:8000/repo/ubuntu@sha256:cb96ec8eb632c873d5130053cf5e2548234e5275d8115a39394289d96c9963a6', 
+     'localhost:8000/repo/ubuntu@sha256:cb96ec8eb632c873d5130053cf5e2548234e5275d8115a39394289d96c9963a6'),
+    ('localhost:8000/path/repo/ubuntu@sha256:cb96ec8eb632c873d5130053cf5e2548234e5275d8115a39394289d96c9963a6', 
+     'localhost:8000/path/repo/ubuntu@sha256:cb96ec8eb632c873d5130053cf5e2548234e5275d8115a39394289d96c9963a6'),
+])
+
+def test_normalize_ref(ref, normalized):
+    assert normalize_ref(ref) == normalized
+
---
+++ plugins/beiran_package_docker/image_ref_parse.py
@@ -0,0 +1,69 @@
+"""
+Parse and normalize image reference
+"""
+
+from typing import Tuple
+
+DEFAULT_DOMAIN = "docker.io"
+OFFICIAL_REPO = "library"
+DEFAULT_TAG = "latest"
+
+
+def normalize_ref(ref: str) -> str:
+    """Parse and normalize image reference.
+    """
+    splitted_ref = ref.split("/")
+    if len(splitted_ref) == 1: # nginx, nginx:0.1
+        domain = DEFAULT_DOMAIN
+        path_comp = OFFICIAL_REPO
+        name, sign, suffix = split_name_suffix(splitted_ref[0])
+
+    elif len(splitted_ref) == 2: # repo/nginx, repo/nginx:0.1
+        domain = DEFAULT_DOMAIN
+        path_comp = splitted_ref[0]
+        name, sign, suffix = split_name_suffix(splitted_ref[1])
+
+    else:
+        if is_domain(splitted_ref[0]):
+            domain = splitted_ref[0]
+            path_comp = "/".join(splitted_ref[1:-1])
+        else:
+            domain = DEFAULT_DOMAIN
+            path_comp = "/".join(splitted_ref[0:-1])
+
+        name, sign, suffix = split_name_suffix(splitted_ref[-1])
+
+    return marshal(domain, path_comp, name, sign, suffix)
+
+
+def split_name_suffix(string: str) -> Tuple[str, str, str]:
+    """Split name and tag (digest)
+    Args:
+        string (str): must be <name>, <name>:<tag> or <name>@<digest>
+    """
+    sign = "@"
+    splitted = string.split(sign)
+    if len(splitted) != 2:
+        # the string is <name>:<tag> or <name>
+        sign = ":"
+        splitted = string.split(sign)
+        if len(splitted) == 1:
+            return splitted[0], sign, DEFAULT_TAG
+    return splitted[0], sign, splitted[1]
+
+
+def is_domain(string: str) -> bool:
+    """Judge whether or not the character string is a domain.
+    Args:
+        string (str): prefix of reference (if reference is p1/p2/r/nginx, arg must be 'p1')
+    """
+    if ":" in string or "." in string:
+        return True
+    return False
+
+
+def marshal(domain: str, path_comp: str, name: str, sign: str, suffix: str) -> str:
+    """
+    Marshall components of reference, and return normalized reference
+    """
+    return domain + "/" + path_comp + "/" + name + sign + suffix

924f8757f497c1900c75b2d0c25545bf33d3a98c
ali <ali@sifirbir.xyz>
2018-11-29 01:20:42 +0000

    pyinstaller remove the hook which become unnecessary since we stop using pkgutil, pyinstaller spec modified, new hook for docker package

--- pyinstaller/hooks/hook-beiran.plugin.py
+++ pyinstaller/hooks/hook-beiran.plugin.py
@@ -1,44 +1,30 @@
 """
 Static replacement of some modules which loads plugin's modules
 and submodules for pyinstaller.
+
+All are commented, no need anymore, since stopped using pkgutil
+for dynamic loading, keep example codes how to override objects
+on the run time.
 """
 
-from beiran import plugin
-from beiran.cli import BeiranCLI
-
-
-def _get_installed_plugins():
-    """
-    Replaces `beiran.plugin.get_installed_plugins` method.
-
-    Returns:
-        list: list of package name of installed beiran plugins.
-
-    """
-    return [
-        "beiran_package_docker",
-    ]
-
-
-plugin.get_installed_plugins = _get_installed_plugins
-
-
-def _list_commands(self, ctx):  # type: ignore
-    """
-    Replaces `beiran.cli.BeiranCLI.list_commands` method.
-
-    Args:
-        ctx (BeiranContext): context object
-
-    Returns:
-        list: list of subcommand names
-
-    """
-    commands = list()
-    commands.append("docker")
-    commands.append("node")
-    commands.sort()
-    return commands
-
-
-BeiranCLI.list_commands = _list_commands
+# from beiran.cli import BeiranCLI
+#
+# def _list_commands(self, ctx):  # type: ignore
+#     """
+#     Replaces `beiran.cli.BeiranCLI.list_commands` method.
+#
+#     Args:
+#         ctx (BeiranContext): context object
+#
+#     Returns:
+#         list: list of subcommand names
+#
+#     """
+#     commands = list()
+#     commands.append("docker")
+#     commands.append("node")
+#     commands.sort()
+#     return commands
+#
+#
+# BeiranCLI.list_commands = _list_commands

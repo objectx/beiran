8bd947ee8cc269f4882e9c15f0a35e645d01c16e
ali <ali@sifirbir.xyz>
2018-12-10 17:36:13 +0000

    doc, add sphinx-click to autodoc click apps

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -40,7 +40,9 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.ifconfig',
               'sphinx.ext.viewcode',
               'sphinx.ext.autodoc',
-              'sphinxcontrib.napoleon']
+              'sphinxcontrib.napoleon',
+              'sphinx_click.ext',
+             ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
